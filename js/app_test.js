ab99ae6fd8229706676eeadd74cf52379ca58466
Marcin Warpechowski <warpech@gmail.com>
2012-10-25 14:08:36 +0200

    rename controller to app_test.js

---
+++ js/app_test.js
@@ -0,0 +1,16 @@
+function MyCtrl($scope, $http, $location) {
+  $scope.clientChange = function () {
+    $scope.FirstName = "Changed By";
+    $scope.LastName = "Controller";
+  }
+
+  $scope.$watch('FirstName', function (value) {
+    console.log("First name is now:", value);
+  });
+
+  $scope.addLamp = function () {
+    $scope.Lamps.push({
+      State: false
+    });
+  };
+}
