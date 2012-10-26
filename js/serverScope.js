61c065e4c5b1ccd332e0b52d9faaa0a650d62e7a
Marcin Warpechowski <warpech@gmail.com>
2012-10-26 15:55:16 +0200

    merge Christian's change for discovering watchers and avoiding sending repeated patches; apply NetBeans code autoformatting

--- js/serverScope.js
+++ js/serverScope.js
@@ -1,101 +1,120 @@
 angular.module('StarcounterLib', ['panelApp'])
   .directive('serverScope', ['$http', 'appContext', function ($http, appContext) {
-  var directiveDefinitionObject = {
-    restrict: 'A',
-    compile: function compile(tElement, tAttrs, transclude) {
+    var directiveDefinitionObject = {
+      restrict: 'A',
+      compile: function compile(tElement, tAttrs, transclude) {
 
-      var rootLoaded = false;
+        var rootLoaded = false;
 
-      function overwriteRoot(scope, data) {
-        for (var i in data) {
-          if (data.hasOwnProperty(i)) {
-            scope[i] = data[i];
+        function overwriteRoot(scope, data) {
+          for (var i in data) {
+            if (data.hasOwnProperty(i)) {
+              scope[i] = data[i];
+            }
+          }
+          findAndSetWatchers(scope);
+        }
+
+        function patchRoot(scope, patch) {
+          if (patch.length) {
+            console.log("patch", patch);
+            jsonpatch.apply(scope, patch);
           }
         }
-      }
 
-      function patchRoot(scope, patch) {
-        if (patch.length) {
-          console.log("patch", patch);
-          jsonpatch.apply(scope, patch);
-        }
-      }
-
-      function parseViewModelId(scope) {
-        var meta = document.getElementsByTagName('meta');
-        for (var i = 0, ilen = meta.length; i < ilen; i++) {
-          if (angular.element(meta[i]).attr('name') == 'View-Model') {
-            scope['View-Model'] = angular.element(meta[i]).attr('content');
-            break;
-          }
-        }
-      }
-
-      function getRequestUrl(scope) {
-        return window.location.href + '/../__vm/' + scope['View-Model'];
-      }
-
-      function getRoot(scope) {
-        $http({method: 'GET', url: getRequestUrl(scope)}).success(function (data, status, headers, config) {
-          overwriteRoot(scope, data);
-          rootLoaded = true;
-        });
-      }
-
-      function updateServer(scope, path, value) {
-        var data = {
-          "replace": path,
-          "value": value
-        };
-        $http({method: 'PATCH', url: getRequestUrl(scope), data: data}).success(function (data, status, headers, config) {
-          patchRoot(scope, data);
-        });
-      }
-
-      function setWatchers(scope, props) {
-        for (var i = 0, ilen = props.length; i < ilen; i++) {
-          scope.$watch(props[i], (function (prop) {
-            return (function (current, previous, scope) {
-              if (rootLoaded) {
-                updateServer(scope, '/' + prop.replace(/\./g, '/'), current);
-              }
-            })
-          })(props[i]), true);
-        }
-      }
-
-      return function postLink(scope, element, attrs, controller) {
-        if (typeof window.__elim_rq !== 'undefined') {
-          overwriteRoot(scope, window.__elim_rq);
-          rootLoaded = true;
-        }
-        else {
-          parseViewModelId(scope);
-          getRoot(scope);
-        }
-
-        var tree = appContext.getScopeTree(scope);
-        var watched = [];
-
-        function findWatchedRecursive(watched, obj, parent) {
-          parent = parent || '';
-          for (var i in obj) {
-            if (obj.hasOwnProperty(i)) {
-              if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
-                findWatchedRecursive(watched, obj[i], parent + i + '.');
-              }
-              else if (typeof obj[i] !== "function") {
-                watched.push(parent + i);
-              }
+        function parseViewModelId(scope) {
+          var meta = document.getElementsByTagName('meta');
+          for (var i = 0, ilen = meta.length; i < ilen; i++) {
+            if (angular.element(meta[i]).attr('name') == 'View-Model') {
+              scope['View-Model'] = angular.element(meta[i]).attr('content');
+              break;
             }
           }
         }
 
-        findWatchedRecursive(watched, tree.locals);
+        function getRequestUrl(scope) {
+          return window.location.href + '/../__vm/' + scope['View-Model'];
+        }
 
-        setWatchers(scope, watched);
+        function getRoot(scope) {
+          $http({
+            method: 'GET', 
+            url: getRequestUrl(scope)
+            }).success(function (data, status, headers, config) {
+            overwriteRoot(scope, data);
+            rootLoaded = true;
+          });
+        }
+
+        function updateServer(scope, path, value) {
+          var data = {
+            "replace": path,
+            "value": value
+          };
+          $http({
+            method: 'PATCH', 
+            url: getRequestUrl(scope), 
+            data: data
+          }).success(function (data, status, headers, config) {
+            patchRoot(scope, data);
+          });
+        }
+
+        function setWatchers(scope, props) {
+          for (var i = 0, ilen = props.length; i < ilen; i++) {
+            scope.$watch(props[i], (function (prop) {
+              return (function (current, previous, scope) {
+                if (rootLoaded) {
+                  // Quick fix for not sending patch to server when watch is set.
+                  if (current === previous) {
+                    return;
+                  }
+                  updateServer(scope, '/' + prop.replace(/\./g, '/'), current);
+                }
+              })
+            })(props[i]), true);
+          }
+        }
+
+        return function postLink(scope, element, attrs, controller) {
+          if (typeof window.__elim_rq !== 'undefined') {
+            overwriteRoot(scope, window.__elim_rq);
+            rootLoaded = true;
+          }
+          else {
+            parseViewModelId(scope);
+            getRoot(scope);
+          }
+        }
+
+        function findAndSetWatchers(scope) {
+          var tree = appContext.getScopeTree(scope);
+          var watched = [];
+
+          function findWatchedRecursive(watched, obj, parent) {
+            parent = parent || '';
+            for (var i in obj) {
+              if (i == "View-Model") continue;
+
+              if (obj.hasOwnProperty(i)) {
+                if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
+                  findWatchedRecursive(watched, obj[i], parent + i + '.');
+                }
+                else if (Object.prototype.toString.apply(obj[i]) === '[object Array]') {
+                  findWatchedRecursive(watched, obj[i], parent + i + '.');
+                }
+                else if (typeof obj[i] !== "function") {
+                  watched.push(parent + i);
+                }
+              }
+            }
+          }
+
+          findWatchedRecursive(watched, tree.locals);
+          setWatchers(scope, watched);
+        }
+
       }
-    }
-  };
-  return directiveDefinitionObject;
-}]);
+    };
+    return directiveDefinitionObject;
+  }]);
