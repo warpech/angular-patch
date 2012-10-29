bfcc2adc9acfaea0597384de918443f11c7c4635
Marcin Warpechowski <warpech@gmail.com>
2012-10-29 11:57:37 +0100

    McOrder app initial release

--- app/mcorder.php
+++ app/mcorder.php
@@ -1,9 +1,66 @@
 <?php
 
 function applicationStart() {
-
+  $items = getProp('Items');
+  array_push($items, getProductSchema()); //add empty row to the table
+  setProp('Items', $items);
 }
 
 function applicationLogic() {
-  
+  if (getProp('AddRow$') === true) {
+    $items = getProp('Items');
+    array_push($items, getProductSchema()); //add empty row to the table
+    setProp('Items', $items);
+    setProp('AddRow$', false);
+  }
+
+  $count = 0;
+  foreach (getProp('Items') as $item) {
+    $count += (int) $item['Quantity$'];
+  }
+  setProp('TotalQuantity', $count);
+  $firephp = FirePHP::getInstance(true);
+  $firephp->log(getProp('Items'), 'php items');
 }
+
+function getProductSchema() {
+  global $jsonPath, $appName;
+  $jsonFile = file_get_contents($jsonPath);
+  $data = json_decode($jsonFile, true);
+  $schema = nullify($data['Items'][0]);
+  return $schema;
+}
+
+function nullify($obj) {
+  switch (gettype($obj)) {
+    case 'array':
+      $return = array();
+      foreach (array_keys($obj) as $key) {
+        $return[$key] = nullify($obj[$key]);
+      }
+      return $return;
+      break;
+
+    default:
+      return null;
+  }
+}
+
+/*
+{
+    "Product": {
+      "_Search$":"Big M",
+      "_Options": [
+      {
+        "Description":"Big Mac", 
+        "Image":"bigmac.png", 
+        "Pick$":null
+      },
+      {
+        "Description":"Big Mac & Co", 
+        "Image":"bigmac_co.png", 
+        "Pick$":null
+      }					
+      ]
+    }
+ */
